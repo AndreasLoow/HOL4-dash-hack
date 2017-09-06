bae24599ba58fc85eac68af64557d8e3be1fc946
Andreas Lööw <AndreasLoow@users.noreply.github.com>
2017-09-06 23:21:00 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # HOL4-dash-hack
-A simple hack to build a HOL4 docset for Dash
+
+A simple hack to build a HOL4 docset for [Dash](https://kapeli.com/dash).
+
+Make sure that you are in the `help` directory in your HOL4 directory. Copy `dashgen.py` and `Info.plist` from this repository and do:
+
+    python3 -m venv venv
+    source ./venv/bin/activate
+    pip install BeautifulSoup4 lxml
+    ./dashgen.py
+
+In case you need to re-generate the docset just do `rm -r HOL4.docset` and run `dashgen.py` again.
+
+To import the docset into Dash: The big "Manage Docsets" button -> the "Docsets" tab -> the small plus button in the lower-left corner -> "Add Local Docset" -> pick the `HOL4.docset` folder in your `help` folder.
---
+++ dashgen.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+# Required packages: BeautifulSoup4 lxml
+
+import shutil
+import sqlite3
+import os
+
+from bs4 import BeautifulSoup
+
+def scrape_index(path, ty):
+    ret = []
+
+    with open(path) as fp:
+        tree = BeautifulSoup(fp, "lxml")
+
+        for e in tree.find_all("li"):
+            bs = e.find_all("b")
+            assert(len(bs) == 1)
+
+            for a in e.find_all("a"):
+                ret.append((a.text + "." + bs[0].text, ty, a.get("href")))
+
+    return ret
+
+
+def copy_and_replace_str(src, dst, needle, repstr):
+    with open(src) as fr:
+        fc = fr.read()
+
+    fc = fc.replace(needle, repstr)
+
+    with open(dst, "w") as fw:
+        fw.write(fc)
+
+
+print("Copying HTML files (1)...")
+# Directory must not exists
+shutil.copytree("Docfiles/HTML", "HOL4.docset/Contents/Resources/Documents/Docfiles")
+
+print("Copying HTML files (2)...")
+# Need to hack some links to "Docfiles"
+#shutil.copytree("src-sml/htmlsigs/", "HOL4.docset/Contents/Resources/Documents")
+
+basedelpath = "file://" + os.path.dirname(os.path.realpath(__file__))
+delpath = basedelpath + "/Docfiles/HTML/"
+
+for f in os.listdir("src-sml/htmlsigs"):
+    copy_and_replace_str(os.path.join("src-sml/htmlsigs", f),
+                         os.path.join("HOL4.docset/Contents/Resources/Documents", f),
+                         delpath,
+                         "Docfiles/")
+
+shutil.copy("Docfiles/doc.css", "HOL4.docset/Contents/Resources/Documents")
+
+copy_and_replace_str("HOLindex.html", "HOL4.docset/Contents/Resources/Documents/index.html",
+                     basedelpath + "/src-sml/htmlsigs/", "")
+
+shutil.copy("Info.plist", "HOL4.docset/Contents")
+
+print("Building index...")
+idx = []
+idx.extend(scrape_index("src-sml/htmlsigs/idIndex.html", "Interface"))
+idx.extend(scrape_index("src-sml/htmlsigs/TheoryIndex.html", "Environment"))
+
+print("Creating database...")
+conn = sqlite3.connect("HOL4.docset/Contents/Resources/docSet.dsidx")
+c = conn.cursor()
+
+c.execute("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT)")
+c.execute("CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path)")
+conn.commit()
+
+c.executemany("INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?, ?, ?)", idx)
+conn.commit()
+
+conn.close()
